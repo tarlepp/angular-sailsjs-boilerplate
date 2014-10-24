c1c1b8fb480ff7133b679583519d6edffacf8e46
tarlepp <tarmo.leppanen@kase.fi>
2014-10-24 17:47:59 +0300

    Added search filter and items per page to Book and Author list GUIs.

--- frontend/src/app/book/book-controllers.js
+++ frontend/src/app/book/book-controllers.js
@@ -28,12 +28,15 @@
     angular.module('frontend.example.book')
         .controller('BookListController',
             [
-                '$scope', '$q',
+                '$scope', '$q', '$timeout',
+                '_',
                 'ListConfig',
-                'BookModel',
-                function($scope, $q,
-                         ListConfig,
-                         BookModel
+                'SocketWhereCondition', 'BookModel',
+                function(
+                    $scope, $q, $timeout,
+                    _,
+                    ListConfig,
+                    SocketWhereCondition, BookModel
                 ) {
                     // Initialize data
                     $scope.endPoint = 'book';
@@ -50,6 +53,12 @@
                         direction: false
                     };
 
+                    // Initialize filters
+                    $scope.filters = {
+                        searchWord: '',
+                        columns: $scope.titleItems
+                    };
+
                     // Function to change sort column / direction on list
                     $scope.changeSort = function changeSort(item) {
                         var sort = $scope.sort;
@@ -61,24 +70,86 @@
                             sort.direction = true;
                         }
 
-                        if ($scope.currentPage === 1) {
-                            fetchData();
-                        } else {
-                            $scope.currentPage = 1;
-                        }
+                        _triggerFetchData();
                     };
 
-                    // Watcher for current page attribute, whenever this changes we need to fetch data from server
-                    $scope.$watch('currentPage', function watcherCurrentPage() {
-                        fetchData();
+                    /**
+                     * Simple watcher for 'currentPage' scope variable. If this is changed we need to fetch book data
+                     * from server.
+                     */
+                    $scope.$watch('currentPage', function watcher() {
+                        _fetchData();
                     });
 
                     /**
-                     * Helper function to fetch necessary data for GUI with currently
-                     * activated sort / page.
+                     * Simple watcher for 'itemsPerPage' scope variable. If this is changed we need to fetch book data
+                     * from server.
                      */
-                    function fetchData() {
+                    $scope.$watch('itemsPerPage', function watcher(valueNew, valueOld) {
+                        if (valueNew !== valueOld) {
+                            _triggerFetchData();
+                        }
+                    });
+
+                    var searchWordTimer;
+
+                    /**
+                     * Watcher for 'filter' scope variable, which contains multiple values that we're interested
+                     * within actual GUI. This will trigger new data fetch query to server if following conditions
+                     * have been met:
+                     *
+                     *  1) Actual filter variable is different than old one
+                     *  2) Search word have not been changed in 400ms
+                     *
+                     * If those are ok, then watcher will call 'fetchData' function.
+                     */
+                    $scope.$watch('filters', function watcher(valueNew, valueOld) {
+                        if (valueNew !== valueOld) {
+                            if (searchWordTimer) {
+                                $timeout.cancel(searchWordTimer);
+                            }
+
+                            searchWordTimer = $timeout(_triggerFetchData, 400);
+                        }
+                    }, true);
+
+                    /**
+                     * Helper function to trigger actual data fetch from backend. This will just check current page
+                     * scope variable and if it is 1 call 'fetchData' function right away. Any other case just set
+                     * 'currentPage' scope variable to 1, which will trigger watcher to fetch data.
+                     *
+                     * @private
+                     */
+                    function _triggerFetchData() {
+                        if ($scope.currentPage === 1) {
+                            _fetchData();
+                        } else {
+                            $scope.currentPage = 1;
+                        }
+                    }
+
+                    /**
+                     * Helper function to fetch actual data for GUI from backend server with current parameters:
+                     *  1) Current page
+                     *  2) Search word
+                     *  3) Sort order
+                     *  4) Items per page
+                     *
+                     * Actually this function is doing two request to backend:
+                     *  1) Data count by given filter parameters
+                     *  2) Actual data fetch for current page with filter parameters
+                     *
+                     * These are fetched via 'BookModel' service with promises.
+                     *
+                     * @private
+                     */
+                    function _fetchData() {
                         $scope.loading = true;
+
+                        // Common parameters for count and data query
+                        var commonParameters = {
+                            where: SocketWhereCondition.get($scope.filters)
+                        };
 
                         // Data query specified parameters
                         var parameters = {
@@ -90,18 +161,18 @@
 
                         // Fetch data count
                         var count = BookModel
-                            .count()
+                            .count(commonParameters)
                             .then(
-                                function successCallback(response) {
+                                function callback(response) {
                                     $scope.itemCount = response.count;
                                 }
                             );
 
                         // Fetch actual data
                         var load = BookModel
-                            .load(parameters)
+                            .load(_.merge({}, commonParameters, parameters))
                             .then(
-                                function successCallback(response) {
+                                function callback(response) {
                                     $scope.items = response;
                                 }
                             );
