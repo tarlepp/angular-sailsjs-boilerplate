6d87d19cf56898b95d85f97066b4b89eafb3e0e9
tarlepp <tarmo.leppanen@kase.fi>
2014-11-01 18:57:48 +0200

    Refactored KeyPress directive.

--- frontend/src/app/components/Directives/KeyPress.js
+++ frontend/src/app/components/Directives/KeyPress.js
@@ -1,34 +1,41 @@
 /**
- * Directive to bind some scope method to certain element and key code. Example this directive
- * can be used to submit form data from input fields by pressing [enter].
+ * Directive to bind some scope method to certain element and key code. Example this directive can be used to submit
+ * form data from input fields by pressing [enter].
  *
  * Usage example:
  *
  *  <input id="message" name="message" class="form-control" type="text"
  *      data-key-press="postMessage()"
  *      data-key-code="13"
+ *      data-timeout="400"
  *      data-ng-model="message.message"
  *  />
  *
- * Example above triggers 'postMessage' scope function when user presses [enter] key.
- *
- * @todo Do we need some timeout between key presses?
+ * Example above triggers 'postMessage' scope function when user presses [enter] key after 400ms waiting. Also note
+ * that data-timeout attribute is not required. If it has
  */
 (function() {
     'use strict';
 
     angular.module('frontend.directives')
-        .directive('keyPress', function() {
-            return function(scope, element, attributes) {
-                element.bind('keydown keypress', function(event) {
-                    if (event.which === parseInt(attributes.keyCode)) {
-                        scope.$apply(function() {
-                            scope.$eval(attributes.keyPress, {'event': event});
-                        });
+        .directive('keyPress',
+            [
+                '$timeout',
+                function($timeout) {
+                    return function(scope, element, attributes) {
+                        element.bind('keydown keypress', function(event) {
+                            if (event.which === parseInt(attributes.keyCode, 10)) {
+                                $timeout(function timeout() {
+                                    scope.$apply(function apply() {
+                                        scope.$eval(attributes.keyPress, {event: event});
+                                    });
+                                }, parseInt(attributes.timeout || 0, 10));
 
-                        event.preventDefault();
-                    }
-                });
-            };
-        });
+                                event.preventDefault();
+                            }
+                        });
+                    };
+                }
+            ]
+        );
 }());
