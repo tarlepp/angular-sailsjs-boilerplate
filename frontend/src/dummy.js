7a0875d0390b42696bd2048d94c51e2c5ebbf02d
tarlepp <tarmo.leppanen@kase.fi>
2014-11-07 19:14:29 +0200

    Added dummy.js for smart IDEs like php/webStorm to enable autocomplete with json data.

---
+++ frontend/src/dummy.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Dummy JS file for smart IDEs like php/webStorm.
+ *
+ * Purpose of this file is to help IDE to use autocomplete features.
+ */
+
+var layout = {
+    menuItem: {
+        state: '{string}',
+        title: '{string}',
+        admin: '{boolean}',
+        items: 'layout.menuItem[]'
+    }
+};
