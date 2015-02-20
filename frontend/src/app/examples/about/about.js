c7792de57e78c12a5ec3fe3e7b44f336adcab30b
tarlepp <tarmo.leppanen@kase.fi>
2015-02-20 23:59:43 +0200

    Code reformat to respect to https://github.com/airbnb/javascript guidelines.

--- frontend/src/app/examples/about/about.js
+++ frontend/src/app/examples/about/about.js
@@ -3,33 +3,31 @@
  * and route definitions for this module.
  */
 (function() {
-    'use strict';
+  'use strict';
 
-    // Define frontend.public module
-    angular.module('frontend.examples.about', []);
+  // Define frontend.public module
+  angular.module('frontend.examples.about', []);
 
-    // Module configuration
-    angular.module('frontend.examples.about')
-        .config(
-            [
-                '$stateProvider',
-                function($stateProvider) {
-                    $stateProvider
-                        .state('examples.about', {
-                            url: '/about',
-                            data: {
-                                access: 0
-                            },
-                            views: {
-                                'content@': {
-                                    templateUrl: '/frontend/examples/about/about.html'
-                                },
-                                'pageNavigation@': false
-                            }
-                        })
-                    ;
-                }
-            ]
-        );
-
+  // Module configuration
+  angular.module('frontend.examples.about')
+    .config([
+      '$stateProvider',
+      function($stateProvider) {
+        $stateProvider
+          .state('examples.about', {
+            url: '/about',
+            data: {
+              access: 0
+            },
+            views: {
+              'content@': {
+                templateUrl: '/frontend/examples/about/about.html'
+              },
+              'pageNavigation@': false
+            }
+          })
+        ;
+      }
+    ])
+  ;
 }());
