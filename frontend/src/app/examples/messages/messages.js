18650e177b9e981db380da9bbbda04e18b79a2c7
tarlepp <tarmo.leppanen@kase.fi>
2015-02-21 13:08:15 +0200

    Code reformat to respect to https://github.com/airbnb/javascript guidelines.

--- frontend/src/app/examples/messages/messages.js
+++ frontend/src/app/examples/messages/messages.js
@@ -6,30 +6,29 @@
  * All of these are wrapped to 'frontend.examples.messages' angular module.
  */
 (function() {
-    'use strict';
+  'use strict';
 
-    // Define frontend.examples.messages angular module
-    angular.module('frontend.examples.messages', []);
+  // Define frontend.examples.messages angular module
+  angular.module('frontend.examples.messages', []);
 
-    // Module configuration
-    angular.module('frontend.examples.messages')
-        .config(
-        [
-            '$stateProvider',
-            function($stateProvider) {
-                $stateProvider
-                    // Messages
-                    .state('examples.messages', {
-                        url: '/examples/messages',
-                        views: {
-                            'content@': {
-                                templateUrl: '/frontend/examples/messages/messages.html',
-                                controller: 'MessagesController'
-                            }
-                        }
-                    })
-                ;
+  // Module configuration
+  angular.module('frontend.examples.messages')
+    .config([
+      '$stateProvider',
+      function config($stateProvider) {
+        $stateProvider
+          // Messages
+          .state('examples.messages', {
+            url: '/examples/messages',
+            views: {
+              'content@': {
+                templateUrl: '/frontend/examples/messages/messages.html',
+                controller: 'MessagesController'
+              }
             }
-        ]
-    );
+          })
+        ;
+      }
+    ])
+  ;
 }());
