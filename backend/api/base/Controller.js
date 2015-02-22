e247e01ad9147f1ab51d640e5ce99f68ef906df3
tarlepp <tarmo.leppanen@kase.fi>
2015-02-22 22:51:59 +0200

    Code reformat to respect to https://github.com/airbnb/javascript guidelines.

--- backend/api/base/Controller.js
+++ backend/api/base/Controller.js
@@ -9,23 +9,24 @@ var actionUtil = require('sails/lib/hooks/blueprints/actionUtil');
  * that every controller uses
  */
 module.exports = {
-    /**
-     * Generic count action for controller.
-     *
-     * @param   {Request}   request
-     * @param   {Response}  response
-     */
-    count: function count(request, response) {
-        var Model = actionUtil.parseModel(request);
+  /**
+   * Generic count action for controller.
+   *
+   * @param   {Request}   request
+   * @param   {Response}  response
+   */
+  count: function count(request, response) {
+    var Model = actionUtil.parseModel(request);
 
-        Model
-            .count(actionUtil.parseCriteria(request))
-            .exec(function found(error, count) {
-                if (error) {
-                    response.negotiate(error);
-                } else {
-                    response.ok({count: count});
-                }
-            });
-    }
+    Model
+      .count(actionUtil.parseCriteria(request))
+      .exec(function found(error, count) {
+        if (error) {
+          response.negotiate(error);
+        } else {
+          response.ok({count: count});
+        }
+      })
+    ;
+  }
 };
--- backend/api/base/Model.js
+++ backend/api/base/Model.js
@@ -6,33 +6,33 @@
  * Base model for all sails.js models. This just contains some common code that every "nearly" every model uses.
  */
 module.exports = {
-    schema: true,
+  schema: true,
 
-    attributes: {
-        // Relation to User object via created user id
-        createdUser: {
-            model:      'User',
-            columnName: 'createdUserId',
-            required:   true
-        },
-        // Relation to User object via updated user id
-        updatedUser: {
-            model:      'User',
-            columnName: 'updatedUserId',
-            required:   true
-        },
+  attributes: {
+    // Relation to User object via created user id
+    createdUser: {
+      model: 'User',
+      columnName: 'createdUserId',
+      required: true
+    },
+    // Relation to User object via updated user id
+    updatedUser: {
+      model: 'User',
+      columnName: 'updatedUserId',
+      required: true
+    },
 
-        // Dynamic model data attributes
+    // Dynamic model data attributes
 
-        // Created timestamp as moment object
-        createdAtObject: function() {
-            return (this.createdAt && this.createdAt != '0000-00-00 00:00:00')
-                ? sails.services['date'].convertDateObjectToUtc(this.createdAt) : null;
-        },
-        // Updated timestamp as moment object
-        updatedAtObject: function() {
-            return (this.updatedAt && this.updatedAt != '0000-00-00 00:00:00')
-                ? sails.services['date'].convertDateObjectToUtc(this.updatedAt) : null;
-        }
+    // Created timestamp as moment object
+    createdAtObject: function() {
+      return (this.createdAt && this.createdAt != '0000-00-00 00:00:00')
+        ? sails.services['date'].convertDateObjectToUtc(this.createdAt) : null;
+    },
+    // Updated timestamp as moment object
+    updatedAtObject: function() {
+      return (this.updatedAt && this.updatedAt != '0000-00-00 00:00:00')
+        ? sails.services['date'].convertDateObjectToUtc(this.updatedAt) : null;
     }
+  }
 };
