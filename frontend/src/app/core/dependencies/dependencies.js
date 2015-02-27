037b61c703f42719721f4efc211d82757970cf0a
tarlepp <tarmo.leppanen@kase.fi>
2015-02-28 01:48:37 +0200

    Improvement documentation.

--- frontend/src/app/core/dependencies/dependencies.js
+++ frontend/src/app/core/dependencies/dependencies.js
@@ -1,6 +1,9 @@
 /**
  * Generic models angular module initialize. This module contains all 3rd party dependencies that application needs to
  * actually work.
+ *
+ * Also note that this module have to be loaded before any other application modules that have dependencies to these
+ * "core" modules.
  */
 (function() {
   'use strict';
