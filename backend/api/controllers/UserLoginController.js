c4733406b956cb628da4e60ebad9fdc5b6b3d867
tarlepp <tarmo.leppanen@kase.fi>
2015-03-01 17:38:12 +0200

    Added controller for UserLogin API.

---
+++ backend/api/controllers/UserLoginController.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var _ = require('lodash');
+
+/**
+ * UserLoginController
+ *
+ * @description :: Server-side logic for managing user login history data
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+module.exports = _.merge(_.cloneDeep(require('../base/Controller')), {
+});
