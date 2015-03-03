869918bcb83a0d91a102cc53f75f73e74fd02ef5
tarlepp <tarmo.leppanen@kase.fi>
2015-03-03 21:34:05 +0200

    Implement user login history GUI logic.

--- frontend/src/app/admin/loginHistory/login-history-controllers.js
+++ frontend/src/app/admin/loginHistory/login-history-controllers.js
@@ -8,13 +8,15 @@
 
   angular.module('frontend.admin.login-history')
     .controller('LoginHistoryController', [
-      '$scope',
+      '$scope', '$timeout', '$q',
       'ListConfig',
+      'SocketHelperService',
       'LoginHistoryModel',
       '_items', '_count',
       function controller(
-        $scope,
+        $scope, $timeout, $q,
         ListConfig,
+        SocketHelperService,
         LoginHistoryModel,
         _items, _count
       ) {
@@ -33,7 +35,7 @@
 
         // Initialize default sort data
         $scope.sort = {
-          column: 'releaseDate',
+          column: 'createdAt',
           direction: false
         };
 
@@ -42,6 +44,134 @@
           searchWord: '',
           columns: $scope.titleItems
         };
+
+        // Function to change sort column / direction on list
+        $scope.changeSort = function changeSort(item) {
+          var sort = $scope.sort;
+
+          if (sort.column === item.column) {
+            sort.direction = !sort.direction;
+          } else {
+            sort.column = item.column;
+            sort.direction = true;
+          }
+
+          _triggerFetchData();
+        };
+
+        // Simple watcher for 'currentPage' scope variable. If this is changed we need to fetch book data from server.
+        $scope.$watch('currentPage', function watcher(valueNew, valueOld) {
+          if (valueNew !== valueOld) {
+            _fetchData();
+          }
+        });
+
+        // Simple watcher for 'itemsPerPage' scope variable. If this is changed we need to fetch book data from server.
+        $scope.$watch('itemsPerPage', function watcher(valueNew, valueOld) {
+          if (valueNew !== valueOld) {
+            _triggerFetchData();
+          }
+        });
+
+        var searchWordTimer;
+
+        /**
+         * Watcher for 'filter' scope variable, which contains multiple values that we're interested
+         * within actual GUI. This will trigger new data fetch query to server if following conditions
+         * have been met:
+         *
+         *  1) Actual filter variable is different than old one
+         *  2) Search word have not been changed in 400ms
+         *
+         * If those are ok, then watcher will call 'fetchData' function.
+         */
+        $scope.$watch('filters', function watcher(valueNew, valueOld) {
+          if (valueNew !== valueOld) {
+            if (searchWordTimer) {
+              $timeout.cancel(searchWordTimer);
+            }
+
+            searchWordTimer = $timeout(_triggerFetchData, 400);
+          }
+        }, true);
+
+        /**
+         * Helper function to trigger actual data fetch from backend. This will just check current page
+         * scope variable and if it is 1 call 'fetchData' function right away. Any other case just set
+         * 'currentPage' scope variable to 1, which will trigger watcher to fetch data.
+         *
+         * @private
+         */
+        function _triggerFetchData() {
+          if ($scope.currentPage === 1) {
+            _fetchData();
+          } else {
+            $scope.currentPage = 1;
+          }
+        }
+
+        /**
+         * Helper function to fetch actual data for GUI from backend server with current parameters:
+         *  1) Current page
+         *  2) Search word
+         *  3) Sort order
+         *  4) Items per page
+         *
+         * Actually this function is doing two request to backend:
+         *  1) Data count by given filter parameters
+         *  2) Actual data fetch for current page with filter parameters
+         *
+         * These are fetched via 'LoginHistoryModel' service with promises.
+         *
+         * @private
+         */
+        function _fetchData() {
+          $scope.loading = true;
+
+          // Common parameters for count and data query
+          var commonParameters = {
+            where: SocketHelperService.getWhere($scope.filters),
+            populate: 'user'
+          };
+
+          // Data query specified parameters
+          var parameters = {
+            limit: $scope.itemsPerPage,
+            skip: ($scope.currentPage - 1) * $scope.itemsPerPage,
+            sort: $scope.sort.column + ' ' + ($scope.sort.direction ? 'ASC' : 'DESC')
+          };
+
+          // Fetch data count
+          var count = LoginHistoryModel
+            .count(commonParameters)
+            .then(
+              function onSuccess(response) {
+                $scope.itemCount = response.count;
+              }
+            )
+          ;
+
+          // Fetch actual data
+          var load = LoginHistoryModel
+            .load(_.merge({}, commonParameters, parameters))
+            .then(
+              function onSuccess(response) {
+                $scope.items = response;
+              }
+            )
+          ;
+
+          // Load all needed data
+          $q
+            .all([count, load])
+            .finally(
+              function onFinally() {
+                $scope.loaded = true;
+                $scope.loading = false;
+              }
+            )
+          ;
+        }
       }
     ])
   ;
